b6d296470a19b38bb6c1defba309320e0e5e55f4
Thien Bui <thienbui797@gmail.com>
2017-06-11 01:22:10 +1000

    test

--- src/app/Peer.jsx
+++ src/app/Peer.jsx
@@ -1,7 +1,7 @@
 import peer from 'peerjs';
 const Peer = {
 	connect : () => {
-		return new peer({key: 'nb7q2evcs567ds4i'});
+		return new peer({key: 'nb7q2evcs567ds4i',secure:true});
 	}
 }
 export default Peer;
